44c667e05f1f3db0fb9ecbf9d176b62e2bbecb47
TSathvik189 <sathvikreddy5678@gmail.com>
2024-10-01 17:02:55 +0530

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
 public static void main(String [] args){
-System.out.println("hello. you have completed the first project")
+System.out.println("hello. you have completed the first project");
 }
 }
